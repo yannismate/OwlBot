0fc51fef0a19cf3f47bc1393e3f489c62cd7d309
Yannis Matezki <git@yannismate.de>
2021-06-23 20:42:58 +0200

    Implemented DiscordProvider

--- src/main/java/de/yannismate/owlbot/providers/DiscordProvider.java
+++ src/main/java/de/yannismate/owlbot/providers/DiscordProvider.java
@@ -2,12 +2,27 @@ package de.yannismate.owlbot.providers;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
 
 @Singleton
 public class DiscordProvider {
 
-  @Inject
-  private BotSettingsProvider botSettingsProvider;
+  private final DiscordClient client;
+  private final GatewayDiscordClient gateway;
 
+  @Inject
+  public DiscordProvider(BotSettingsProvider botSettingsProvider) {
+    this.client = DiscordClient.create(botSettingsProvider.getDiscordToken());
+    this.gateway = client.login().block();
+  }
+
+  public DiscordClient getClient() {
+    return client;
+  }
+
+  public GatewayDiscordClient getGateway() {
+    return gateway;
+  }
 
 }
