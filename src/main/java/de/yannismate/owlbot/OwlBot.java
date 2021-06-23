bc7965d8d75348f52dd056e19d0bc3384e479b4b
Yannis Matezki <git@yannismate.de>
2021-06-23 17:35:59 +0200

    Initial commit

---
+++ src/main/java/de/yannismate/owlbot/OwlBot.java
@@ -0,0 +1,16 @@
+package de.yannismate.owlbot;
+
+import com.google.inject.AbstractModule;
+
+public class OwlBot extends AbstractModule {
+
+  public static void main(String[] args) {
+
+  }
+
+  @Override
+  protected void configure() {
+    //bind().asEagerSingleton();
+  }
+
+}
