a187024cc8be9d9cd262e10f0849d894719cdbfe
Yannis Matezki <git@yannismate.de>
2021-07-07 00:23:40 +0200

    Added some tests

---
+++ src/test/java/de/yannismate/owlbot/model/GuildSettingsTest.java
@@ -0,0 +1,67 @@
+package de.yannismate.owlbot.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import de.yannismate.owlbot.model.GuildSettings.Permissions;
+import discord4j.common.util.Snowflake;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+import org.bson.Document;
+import org.junit.jupiter.api.Test;
+
+public class GuildSettingsTest {
+
+
+  @Test
+  void testBsonConversion() {
+
+    Snowflake ownerId = Snowflake.of(ThreadLocalRandom.current().nextLong());
+    Snowflake randomRole = Snowflake.of(ThreadLocalRandom.current().nextLong());
+
+    Document testDoc = new Document();
+    testDoc.append("guild_id", Snowflake.of(ThreadLocalRandom.current().nextLong()).asLong());
+    Document perm = new Document();
+    Document userPerm = new Document();
+    Document rolePerm = new Document();
+
+    userPerm.append(ownerId.asString(), List.of("*"));
+    rolePerm.append(randomRole.asString(), List.of("test.permission"));
+    perm.append("user_permissions", userPerm);
+    perm.append("role_permissions", rolePerm);
+    testDoc.append("permissions", perm);
+    Document settings = new Document("prefix", "!");
+    testDoc.append("settings", settings);
+    testDoc.append("enabled_modules", List.of("TestModule"));
+
+    testDoc = Document.parse(testDoc.toJson());
+
+    assertEquals(testDoc, GuildSettings.fromDocument(testDoc).toDocument());
+
+  }
+
+  @Test
+  void testPermissionMatching() {
+
+    Snowflake ownerId = Snowflake.of(ThreadLocalRandom.current().nextLong());
+    Snowflake notOwnerId = Snowflake.of(ownerId.asLong() + 1);
+    Snowflake randomRole = Snowflake.of(ThreadLocalRandom.current().nextLong());
+
+    Permissions permissions = new Permissions();
+    permissions.getUserPermissions().put(ownerId, Set.of("*"));
+    permissions.getRolePermissions().put(randomRole, Set.of("test.*"));
+
+    assertTrue(permissions.hasPermission(ownerId, Collections.emptySet(), "any_permission"));
+    assertTrue(permissions.hasPermission(ownerId, Collections.emptySet(), "test.any_permission"));
+    assertTrue(permissions.hasPermission(ownerId, Collections.singleton(randomRole), "any_permission"));
+
+    assertTrue(permissions.hasPermission(notOwnerId, Collections.singleton(randomRole), "test.any_permission.foo"));
+    assertFalse(permissions.hasPermission(notOwnerId, Collections.singleton(randomRole), "any_permission"));
+
+  }
+
+
+}
---
+++ src/test/java/de/yannismate/owlbot/model/ModuleSettingsTest.java
@@ -0,0 +1,44 @@
+package de.yannismate.owlbot.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import de.yannismate.owlbot.model.ModuleSettings.SettingsObject;
+import discord4j.common.util.Snowflake;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import org.bson.Document;
+import org.junit.jupiter.api.Test;
+
+public class ModuleSettingsTest {
+
+  @Test
+  void testBsonConversion() {
+    Document testDoc = new Document();
+    testDoc.append("guild_id", Snowflake.of(ThreadLocalRandom.current().nextLong()).asLong());
+    testDoc.append("module_name", "TestModule");
+
+    Document settings = new Document();
+    settings.append("key1", SettingsObject.of("value1").get());
+    settings.append("key2", SettingsObject.of(ThreadLocalRandom.current().nextInt()).get());
+    settings.append("key3", SettingsObject.of(ThreadLocalRandom.current().nextLong()).get());
+    settings.append("key4", SettingsObject.of(Map.of("key5", SettingsObject.of("key6"))).get());
+    testDoc.append("settings", settings);
+
+    //Simulate database insert and get
+    testDoc = Document.parse(testDoc.toJson());
+
+    ModuleSettings ms = ModuleSettings.fromDocument(testDoc);
+
+    assertEquals(testDoc, Document.parse(ms.toDocument().toJson()));
+
+    ModuleSettings ms2 = new ModuleSettings(
+        Snowflake.of(ThreadLocalRandom.current().nextLong()),
+        "TestModule2",
+        Map.of("test_key", ModuleSettings.SettingsObject.of(1234),
+            "test_key2", ModuleSettings.SettingsObject.of(Map.of("test_key3", ModuleSettings.SettingsObject.of("1234a")))));
+
+    ms2.toDocument();
+
+  }
+
+}
