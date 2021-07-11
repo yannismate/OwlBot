2dcfc9f54dff376bd80b2fedb7a4daf799b11656
Yannis Matezki <git@yannismate.de>
2021-07-11 20:31:49 +0200

    Added logging for Join/Leave events

--- src/main/java/de/yannismate/owlbot/modules/LoggingModule.java
+++ src/main/java/de/yannismate/owlbot/modules/LoggingModule.java
@@ -7,8 +7,11 @@ import de.yannismate.owlbot.model.db.ModuleSettings;
 import de.yannismate.owlbot.model.db.ModuleSettings.ModuleSettingsValue;
 import de.yannismate.owlbot.services.DatabaseService;
 import de.yannismate.owlbot.services.DiscordService;
+import de.yannismate.owlbot.util.MessageUtils;
 import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.guild.MemberJoinEvent;
+import discord4j.core.event.domain.guild.MemberLeaveEvent;
+import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -29,63 +32,139 @@ public class LoggingModule extends Module {
   public LoggingModule() {
     this.name = "Logging";
     this.description = "Allows logging of joining and leaving users, role changes, bans and command usage.";
+  }
+
+  @Override
+  public void postInit() {
     this.registerEvents();
   }
 
   @Override
-  public void enable(Snowflake guildId) {
+  public void onEnableFor(Snowflake guildId) {
     db.getModuleSettings(guildId, LoggingModule.class.getSimpleName()).thenAccept(moduleSettings -> {
       if(moduleSettings.isPresent()) return;
       ModuleSettings md = new ModuleSettings(guildId, this.getClass().getSimpleName());
       md.getOptions().put("log_member_join", new ModuleSettingsValue(Map.of(
           "enabled", new ModuleSettingsValue(false),
           "channel", new ModuleSettingsValue(-1L),
-          "format", new ModuleSettingsValue("\uD83D\uDCE5 {time} <@{userid}> joined the server. Original name was **{usertag}**. Total members: {membercount}")
+          "format", new ModuleSettingsValue("\uD83D\uDCE5 ${time} <@${userid}> joined the server. Original name was **${usertag}**. Total members: ${membercount}")
       )));
 
       md.getOptions().put("log_member_leave", new ModuleSettingsValue(Map.of(
           "enabled", new ModuleSettingsValue(false),
           "channel", new ModuleSettingsValue(-1L),
-          "format", new ModuleSettingsValue("\uD83D\uDCE4 {time} <@{userid}> left the server. Original name was **{usertag}**. Total members: {membercount}")
+          "format", new ModuleSettingsValue("\uD83D\uDCE4 ${time} <@${userid}> left the server. Original name was **${usertag}**. Total members: ${membercount}")
       )));
 
       md.getOptions().put("log_member_role_change", new ModuleSettingsValue(Map.of(
           "enabled", new ModuleSettingsValue(false),
           "channel", new ModuleSettingsValue(-1L),
-          "format", new ModuleSettingsValue("\uD83D\uDDD2 {time} **{usertag}'s** roles changed: `{rolediff}`")
+          "format", new ModuleSettingsValue("\uD83D\uDDD2 ${time} **${usertag}'s** roles changed: `${rolediff}`")
       )));
 
       md.getOptions().put("log_member_banned", new ModuleSettingsValue(Map.of(
           "enabled", new ModuleSettingsValue(false),
           "channel", new ModuleSettingsValue(-1L),
-          "format", new ModuleSettingsValue("⛔️ {time} <@{userid}> was banned! Original name was **{usertag}**.")
+          "format", new ModuleSettingsValue("⛔️ ${time} <@${userid}> was banned! Original name was **${usertag}**.")
       )));
 
       md.getOptions().put("log_command_used", new ModuleSettingsValue(Map.of(
           "enabled", new ModuleSettingsValue(false),
           "channel", new ModuleSettingsValue(-1L),
-          "format", new ModuleSettingsValue("\uD83D\uDCE5 {time} **{usertag}'s** executed the command: `{command} {args}` in <#{channelid}>")
+          "format", new ModuleSettingsValue("\uD83D\uDCE5 ${time} **${usertag}'s** executed the command: `${command} ${args}` in <#${channelid}>")
       )));
 
       db.addModuleSettings(md);
     });
   }
 
-  @Override
-  public void disable(Snowflake guildId) {
-
-  }
-
   private void registerEvents() {
     this.discordService.getGateway().on(MemberJoinEvent.class).subscribe(memberJoinEvent -> {
+      Snowflake guildId = memberJoinEvent.getGuildId();
       db.getGuildSettings(memberJoinEvent.getGuildId())
           .thenApply(guildSettings -> guildSettings.isPresent() && guildSettings.get().getEnabledModules().contains(LoggingModule.class.getSimpleName()))
           .thenCompose(e -> e ? db.getModuleSettings(memberJoinEvent.getGuildId(), LoggingModule.class.getSimpleName()) : CompletableFuture.completedFuture(Optional.empty()))
           .thenAccept(moduleSettings -> {
 
             if(moduleSettings.isEmpty()) return;
-            ///TODO
-      });
+
+            Map<String, ModuleSettingsValue> eventSettings = moduleSettings.get().getOptions().get("log_member_join").getNested().orElseThrow();
+            if(!(Boolean)eventSettings.get("enabled").getRaw()) return;
+            if(eventSettings.get("channel").getRaw().equals(-1L)) return;
+
+            String format = (String) eventSettings.get("format").getRaw();
+            Snowflake channelId = Snowflake.of((Long) eventSettings.get("channel").getRaw());
+
+            if(format.contains("${membercount}")) {
+              memberJoinEvent.getGuild().subscribe(guild -> {
+                String message = MessageUtils.replaceTokens(format, Map.of(
+                    "time", "<t:" + Instant.now().getEpochSecond() + ">",
+                    "userid", memberJoinEvent.getMember().getId().asString(),
+                    "usertag", memberJoinEvent.getMember().getTag(),
+                    "membercount", "" + guild.getMemberCount()
+                ));
+                discordService.createMessageInChannel(guildId, channelId, message).subscribe();
+
+              });
+
+            } else {
+
+              String message = MessageUtils.replaceTokens(format, Map.of(
+                  "time", "<t:" + Instant.now().getEpochSecond() + ">",
+                  "userid", memberJoinEvent.getMember().getId().asString(),
+                  "usertag", memberJoinEvent.getMember().getTag()
+              ));
+
+              discordService.createMessageInChannel(guildId, channelId, message).subscribe();
+
+            }
+
+          });
+
+    });
+
+    this.discordService.getGateway().on(MemberLeaveEvent.class).subscribe(memberLeaveEvent -> {
+      if(memberLeaveEvent.getMember().isEmpty()) return;
+      Snowflake guildId = memberLeaveEvent.getGuildId();
+      db.getGuildSettings(memberLeaveEvent.getGuildId())
+          .thenApply(guildSettings -> guildSettings.isPresent() && guildSettings.get().getEnabledModules().contains(LoggingModule.class.getSimpleName()))
+          .thenCompose(e -> e ? db.getModuleSettings(memberLeaveEvent.getGuildId(), LoggingModule.class.getSimpleName()) : CompletableFuture.completedFuture(Optional.empty()))
+          .thenAccept(moduleSettings -> {
+
+            if(moduleSettings.isEmpty()) return;
+
+            Map<String, ModuleSettingsValue> eventSettings = moduleSettings.get().getOptions().get("log_member_leave").getNested().orElseThrow();
+            if(!(Boolean)eventSettings.get("enabled").getRaw()) return;
+            if(eventSettings.get("channel").getRaw().equals(-1L)) return;
+
+            String format = (String) eventSettings.get("format").getRaw();
+            Snowflake channelId = Snowflake.of((Long) eventSettings.get("channel").getRaw());
+
+            if(format.contains("${membercount}")) {
+              memberLeaveEvent.getGuild().subscribe(guild -> {
+                String message = MessageUtils.replaceTokens(format, Map.of(
+                    "time", "<t:" + Instant.now().getEpochSecond() + ">",
+                    "userid", memberLeaveEvent.getMember().get().getId().asString(),
+                    "usertag", memberLeaveEvent.getMember().get().getTag(),
+                    "membercount", "" + guild.getMemberCount()
+                ));
+                discordService.createMessageInChannel(guildId, channelId, message).subscribe();
+
+              });
+
+            } else {
+
+              String message = MessageUtils.replaceTokens(format, Map.of(
+                  "time", "<t:" + Instant.now().getEpochSecond() + ">",
+                  "userid", memberLeaveEvent.getMember().get().getId().asString(),
+                  "usertag", memberLeaveEvent.getMember().get().getTag()
+              ));
+
+              discordService.createMessageInChannel(guildId, channelId, message).subscribe();
+
+            }
+
+          });
 
     });
   }
