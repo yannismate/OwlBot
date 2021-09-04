cda222c165a59577c5a083855f730e8872c1d023
Yannis Matezki <git@yannismate.de>
2021-09-04 23:01:47 +0200

    Twitch OAuth token is now preserved

--- src/main/java/de/yannismate/owlbot/services/TwitchAuthenticationService.java
+++ src/main/java/de/yannismate/owlbot/services/TwitchAuthenticationService.java
@@ -2,6 +2,7 @@ package de.yannismate.owlbot.services;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,6 +10,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.file.Files;
 import java.util.concurrent.CompletableFuture;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -19,24 +21,40 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class TwitchAuthenticationService {
 
+  private final static String AUTH_FILE_NAME = "twitch_auth.json";
+
   private final Logger logger = LoggerFactory.getLogger(TwitchAuthenticationService.class);
 
   private final String clientId;
   private final String clientSecret;
 
   private String token;
-  private long tokenExpiresAt;
+  private long tokenExpiresAt = 0;
 
   private final HttpClient client;
 
-  //TODO: Store Tokens in DB and refresh instead of replace
+
   @Inject
   public TwitchAuthenticationService(BotSettingsService botSettingsService)
       throws TwitchRequestException {
     this.clientId = botSettingsService.getTwitchClientId();
     this.clientSecret = botSettingsService.getTwitchClientSecret();
     this.client = HttpClient.newHttpClient();
-    this.requestNewToken();
+    File authFile = new File(AUTH_FILE_NAME);
+    if(authFile.exists()) {
+      try {
+        String json = Files.readString(authFile.toPath());
+        JSONParser parser = new JSONParser();
+        JSONObject authData = (JSONObject) parser.parse(json);
+        this.token = (String) authData.get("token");
+        this.tokenExpiresAt = (long) authData.get("token_expires_at");
+      } catch (IOException | ParseException e) {
+        logger.atError().addArgument(e).log("Could not read existing twitch auth file. {}");
+      }
+    }
+
+    if(token == null || tokenExpiresAt < System.currentTimeMillis()) requestNewToken();
+
   }
 
   private void requestNewToken() throws TwitchRequestException {
@@ -64,9 +82,19 @@ public class TwitchAuthenticationService {
 
     this.tokenExpiresAt = System.currentTimeMillis() + (1000L * (long)responseJson.get("expires_in"));
     this.token = (String) responseJson.get("access_token");
-    logger.info("Successfully requested new Twitch Auth token.");
+    logger.atInfo().log("Successfully requested new Twitch Auth token.");
 
+    JSONObject tokenInfo = new JSONObject();
+    tokenInfo.put("token", this.token);
+    tokenInfo.put("token_expires_at", tokenExpiresAt);
+    try {
+      Files.writeString(new File(AUTH_FILE_NAME).toPath(), tokenInfo.toJSONString());
+    } catch (IOException e) {
+      logger.atError().addArgument(e).log("Could not save twitch auth data in file. {}");
+    }
+    logger.atInfo().log("Successfully saved new token info to file.");
   }
+
 
   public CompletableFuture<String> getAppToken() {
     return CompletableFuture.supplyAsync(() -> {
